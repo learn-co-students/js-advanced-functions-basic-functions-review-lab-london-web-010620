b21c2e40c0aadcd81cd5204008333bc7f26c8165
Harriet Bicknell <harrietindubai@gmail.com>
2020-02-24 08:10:22 +0000

    Done.

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ let wrapAdjective = function(style="*") {
 }
 
 let Calculator = {
-    add: function = (a,b) { return a + b},
+    add: function(a,b) { return a + b},
     subtract: function(a,b) { return a - b},
     multiply: function(a,b) { return a * b},
     divide: function(a,b) { return a / b}
