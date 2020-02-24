1c9b2af5943d6a298a945bd2f0f184569f326fe5
pixulainen <george.apetrei@gmail.com>
2020-02-24 09:36:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+const saturdayFun = (activity = "roller-skate") => { return `This Saturday, I want to ${activity}!` }
+const mondayWork = (activity = "go to the office") => { return `This Monday, I will ${activity}.` }
+const wrapAdjective = (style ="*") => {
+    return (adj = "special") => {
+         return `You are ${style}${adj}${style}!`;
+    }
+}
+const Calculator = {
+    add: (num1, num2) => { return num1 + num2 },
+    subtract: (a, b) => { return a - b },
+    multiply: (a, b) => { return a * b },
+    divide: (a,b) => {return a/b}
+}
+const actionApplyer = (start, array) => {
+   for (let i = 0; i < array.length; i++) {
+       start = array[i](start);
+   }
+    return start
+}
